d908d359d01a7f234383ecc0692ba92ef904bb9a
kaetaen <kaetaen@tutanota.com>
2022-03-10 23:55:45 -0300

    feat: index universities

--- src/app/controllers/UniversityController.js
+++ src/app/controllers/UniversityController.js
@@ -1,12 +1,39 @@
 const University = require('../models/University')
 
 class UniversityController {
-    constructor () {
-        this.university = University
-    }
-
     async index (req, res) {
-        return this.university.find()
+        // Filtro por país
+        const countryName = req.query.country
+        const filter = countryName ? {country: countryName[0].toUpperCase() + countryName.slice(1)} : {}
+
+        // Paginação
+        let page = req.query.page;
+        let limit = 20;
+        let skip = limit * (page - 1);
+        const countUniversity =  await University.count(filter)
+        const totalUniversityPerPage = Math.ceil(countUniversity / limit)
+
+        // Busca por dados
+        University.find(filter, function(err, univer) {
+            if (err)
+                return res.json({"Erro": "Error to list universities !"});
+            else {
+                 const universities = univer.map( (uni) => {
+                   return {
+                       id: uni._id,
+                       name: uni.name,
+                       country: uni.country,
+                       stateProvince: uni["state-province"]
+                    }
+                })
+
+                res.status(200).json({
+                    currentPage:page,
+                    lastPage:totalUniversityPerPage,
+                    dados: universities,
+                }); 
+            }
+        }).skip(skip).limit(limit);
     }   
 
     show () {
